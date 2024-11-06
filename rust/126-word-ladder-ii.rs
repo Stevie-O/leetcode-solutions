82d64633f0b56ce2a5215bb427b9e3554c5e02b1
Stevie-O <stevie@qrpff.net>
2024-11-06 14:30:51 -0500

    Update 126-word-ladder-ii.rs
    this was "new 267"

--- rust/126-word-ladder-ii.rs
+++ rust/126-word-ladder-ii.rs
@@ -1,6 +1,20 @@
-// OOM'd on one input
+// OOM'd on one input (23 of 37, I believe)
 // "cet", "ism", ["kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim"]
 // which is kind of _odd_ because it shouldn't take that much space? 500 usizes doesn't seem like much...
+// AHH it must be my chains/next_chains spreading out very, very far... I wonder what the graph for this looks like...
+/* let's see. _can_ we prune any chains?
+ * let's consider a beginword B, an endword W, and a set of chains C1, C2, C3, ... that all start with B
+ * let's say we're considering a word W to append to C3
+ *    but C1 already contains W
+ * is it possible for (C3+W) to still be a "shortest transformation sequence"?
+ * - let's consider the shortest sequence from W to E.  Can that include any words from C3?
+ *   I'm pretty sure the answer is NO
+ *	 if (C3+E) is valid, then (C3+W) is not a shortest transformation sequence
+ *   furthermore, no subsequence of C3 leads to E, because if we're considering C3, then no sequences of length less than C3 end in E
+ *   therefore, we don't actually need to track "visited" on a per-node basis.
+ *   We really want to track whether or not any given word W is reachable in less than len(C3) steps (including the step to start at B).
+ */
+ 
 
 use std::hash::{Hash, Hasher};
 use std::collections::{HashSet, HashMap};

c5d5e04dbe24e3d71e547faa96b681a45a810856
Stevie-O <stevie@qrpff.net>
2024-11-06 14:47:43 -0500

    Update 16-3sum-closest.rs
    this was "new 300"

--- rust/16-3sum-closest.rs
+++ rust/16-3sum-closest.rs
@@ -1,5 +1,9 @@
-// 16. 3Sum closest
-
+// 16. 3Sum Closest
+// given an array and a target sum,
+// find the closest value to target_sum you can get for any three distinct indices
+// note that ([3, 3, 3], 9) has an exact answer with indices 0, 1, 2
+struct NonCopy<T>(T);
+use core::ops::{Add, Sub};
 
 impl Solution {
     // panics if n contains fewer than 2 elements.
@@ -9,44 +13,72 @@ impl Solution {
     //  1. there exists at least one i, j (i!=j) such that nums[i] + nums[j] == X
     //  2. for all i, j (i!=j) in 0..nums.len(), abs(nums[i] + nums[j] - target) >= abs(X - target)
     // put another way,
-    fn pair_sum_closest<T : Copy + Ord + Add + Sub + Default>(nums: &[T], target: T) -> T {
+    fn pair_sum_closest<T : Copy + Ord + Add<Output=T> + Sub<Output=T> + Default + std::fmt::Debug + std::fmt::Display>(nums: &[T], target: T) -> T {
         assert!(nums.len() >= 2, "pair_sum_closest: input slice is too small (len={})", nums.len());
         let mut best_sum = nums[0] + nums[1];
         let mut best_dist = if best_sum < target { target - best_sum } else { best_sum - target };
         // if the first two elements somehow give a perfect answer, well, 
-        if best_dist == <T as Default>::default() { return best_sum; }
-        for i in 0..nums.len() - 1 {
-            let target_numsj = target - nums[i];
-            let search_result = nums[i+1..].binary_search((|&numsj| numsj.cmp(target_numsj));
-            match pos {
-                Ok(j) => {
-                    // perfect match!
-                    assert_eq!(nums[i] + nums[i+1+j], target);
-                    return target;
-                }
-                Err(j_offs) => {
-                    let j = i + 1 + j_offs;
-                    // well, let's see how close we got, I guess
-                    let slice_start = if j_offs > 0 { j - 1 } else { j };
-                    let slice_end = if j + 1 < nums.len() { j + 1 } else { j };
-                    for try_j in nums[slice_start .. slice_end ] {
-                        let new_sum = nums[i] + nums[j];
-                        let new_dist = if new_sum < target { target - new_sum } else { new_sum - target };
-                        if new_dist < best_dist { (best_sum, best_dist) = (new_sum, new_dist); }
+        let mut prev_j_val : Option<T> = None;
+
+        // i kept using the wrong slice
+        let mut nums = NonCopy(nums);
+
+        while /*nums.0.len() >= 2 && */ let [j_val, rest @ .. ] = nums.0 {
+            if rest.is_empty() { break; }
+            let j_val = *j_val;
+            //let _ : () = j_val;
+            if prev_j_val != Some(j_val) {
+                prev_j_val = Some(j_val);
+                let target_numsk = target - j_val;
+
+                let search_result = rest.binary_search(&target_numsk);
+                match search_result {
+                    Ok(k) => {
+                        // perfect match!
+                        assert_eq!(j_val + rest[k], target);
+                        return target;
+                    }
+                    Err(k) => {
+                        // well, let's see how close we got, I guess
+                        let slice_start = if k > 0 { k - 1 } else { k };
+                        let slice_end = if k + 1 < rest.len() { k + 1 } else { k };
+                        for &k_val in &rest[slice_start .. slice_end] {
+                            let new_sum = j_val + k_val;
+                            let new_dist = if new_sum < target { target - new_sum } else { new_sum - target };
+                            if new_dist < best_dist { (best_sum, best_dist) = (new_sum, new_dist); }
+                        }
                     }
                 }
             }
+            nums = NonCopy(rest);
         }
         best_sum
     }
     pub fn three_sum_closest(mut nums: Vec<i32>, target: i32) -> i32 {
         nums.sort();
+        let nums = nums; // no more mut
         let mut best_sum = nums[0] + nums[1] + nums[2];
-        let mut best_dist = target.abs_diff(&best_sum);
-        let num_slice = 
-        for (i, i_val) in &nums[0..nums.len()-2].into_iter().copied().enumerate(()) {
-            let subsum_target = target - i_val;
-            let found_subsum = Self::pair_sum_closest()
+        let mut best_dist = target.abs_diff(best_sum);
+        let mut previous_i_val = nums[0] - 1;
+
+        let mut num_slice = nums.as_slice();
+        while let [ i_val, rest @ .. ] = num_slice {
+            if rest.len() < 2 { break; }
+            let i_val = *i_val;
+            // if the same number occurs multiple times, don't check it again
+            if i_val != previous_i_val {
+                previous_i_val = i_val;
+
+                let target_subsum = target - i_val;
+                let found_subsum = Self::pair_sum_closest(rest, target_subsum);
+                // if we found an exact match, we can stop here
+                if found_subsum == target_subsum { return target; }
+                let found_diff = target_subsum.abs_diff(found_subsum);
+                if found_diff < best_dist {  (best_sum, best_dist) = (found_subsum + i_val, found_diff); }
+            }
+            num_slice = rest;
         }
+
+        best_sum
     }
 }

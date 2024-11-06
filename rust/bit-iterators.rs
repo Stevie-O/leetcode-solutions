d9044ebbb47af82daa35364fd9210d642ef16515
Stevie-O <stevie@qrpff.net>
2024-11-06 16:23:29 -0500

    Update bit-iterators.rs
    Improved version that doesn't require explicitly naming the iterator type

--- rust/bit-iterators.rs
+++ rust/bit-iterators.rs
@@ -1,12 +1,12 @@
 // allow "x.bit_iter()" for numeric types to return the bit numbers (with LSB=0) that are set in x
-// this was my original implementation. it's a little clunky.
+// this is after I learned a bit -- it uses a single generic type for all iterator implementations
 
 // bit_iter() returns a sequence of u32s containing the bits that are set in the number
 trait IntoBitIterator { type BitIteratorType : Iterator<Item = u32>; fn bit_iter(self) -> Self::BitIteratorType; }
+struct BitIterator<T>(T);
 macro_rules! define_bit_iterator {
-    ($num_type:ty, $iterator_type:ident) => {
-        struct $iterator_type ($num_type);
-        impl Iterator for $iterator_type {
+    ($num_type:ty) => {
+        impl Iterator for BitIterator<$num_type> {
             type Item = u32;
             fn next(&mut self) -> Option<Self::Item> {
                 if self.0 == 0 { None }
@@ -18,19 +18,16 @@ macro_rules! define_bit_iterator {
             }
         }
         impl IntoBitIterator for $num_type {
-            type BitIteratorType = $iterator_type;
+            type BitIteratorType = BitIterator<$num_type>;
             fn bit_iter(self) -> Self::BitIteratorType {
-                $iterator_type(self)
+                BitIterator(self)
             }
         }
     }
 }
-define_bit_iterator!(i16, I16BitIterator);
-define_bit_iterator!(u16, U16BitIterator);
-define_bit_iterator!(u128, U128BitIterator);
-
-
-
+define_bit_iterator!(i16);
+define_bit_iterator!(u16);
+define_bit_iterator!(u128);
 
 fn main() {
    println!("bit iterator on 0xFF_i16: {:?}", (0xFF_i16).bit_iter().collect::<Vec<_>>());
